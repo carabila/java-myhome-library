4aeaab6d603a6267cb94e55751cad0db1cf064a1
carabila <7900775+carabila@users.noreply.github.com>
2017-11-05 11:49:04 +0100

    Update MyHomeSocketFactory.java
    Added OpenWebNet password support.

--- myhome-library/src/com/myhome/fcrisciani/connector/MyHomeSocketFactory.java
+++ myhome-library/src/com/myhome/fcrisciani/connector/MyHomeSocketFactory.java
@@ -11,246 +11,271 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author Flavio Crisciani
  *
  */
 public class MyHomeSocketFactory {
-	// ----- TYPES ----- //
+    // ----- TYPES ----- //
 
-	// ---- MEMBERS ---- //
-	
-	final static String socketCommand = "*99*0##";			// OpenWebNet command to ask for a command session
-	final static String socketMonitor = "*99*1##";			// OpenWebNet command to ask for a monitor session
+    // ---- MEMBERS ---- //
 
-	// ---- METHODS ---- //
+    final static String socketCommand = "*99*0##"; // OpenWebNet command to ask for a command session
+    final static String socketMonitor = "*99*1##"; // OpenWebNet command to ask for a monitor session
 
-	/** 
-	 * Reads a well formed message from the input stream passed and return it back
-	 * @param inputStream steam to read from
-	 * @return the message read
-	 * @throws IOException in case of problem with the input stream, close the stream
-	 */
-	protected static String readUntilDelimiter(final BufferedReader inputStream) throws IOException,SocketTimeoutException{
-		StringBuffer response = new StringBuffer();
-		int ci = 0;
-		char c = ' ';
-		Boolean canc = false;
-		
-		// Cycle that reads one char each cycle and stop when the sequence ends with ## that is the OpenWebNet delimiter of each message
-		do{
-			ci = inputStream.read();		    		
-			if (ci == -1){
-				System.err.println("Socket already closed by server \n");
-				inputStream.close();
-				throw new IOException(); 
-			}else{ 
-				c = (char)ci;
-				if (c == '#' && canc == false){ // Found first #
-					response.append(c);
-					canc = true;
-				}else if (c == '#'){			// Found second # command terminated correctly EXIT
-					response.append(c);
-					break;
-				}else if (c != '#'){			// Append char and start again finding the first #
-					response.append(c);
-					canc = false;
-				}
-			}
-		}while(true);
+    // ---- METHODS ---- //
 
-		return response.toString();
-	}
-	/** 
-	 * Reads multiple messages from the input stream and returns them back in an array
-	 * @param inputStream steam to read from
-	 * @return an array of messages
-	 * @throws IOException in case of problem with the input stream, close the stream
-	 */
-	protected static String[] readUntilAckNack(final BufferedReader inputStream) throws IOException{
-		ArrayList<String> result = new ArrayList<String>();
-		String commandReceived  = null;
-		// Call multiple times the previous function to read more messages.
-		// A sequence of multiple messages end always with an ACK or NACK so stop this cycle when the message is one of them
-		do{
-			commandReceived = readUntilDelimiter(inputStream);
-			result.add(commandReceived);
-		}
-		while(commandReceived!= null && isACK(commandReceived) != true && isNACK(commandReceived) != true);
+    /**
+     * Reads a well formed message from the input stream passed and return it back
+     *
+     * @param inputStream steam to read from
+     * @return the message read
+     * @throws IOException in case of problem with the input stream, close the stream
+     */
+    protected static String readUntilDelimiter(final BufferedReader inputStream)
+            throws IOException, SocketTimeoutException {
+        StringBuffer response = new StringBuffer();
+        int ci = 0;
+        char c = ' ';
+        Boolean canc = false;
 
-		return result.toArray(new String[result.size()]);
-	}
+        // Cycle that reads one char each cycle and stop when the sequence ends with ## that is the OpenWebNet delimiter
+        // of each message
+        do {
+            ci = inputStream.read();
+            if (ci == -1) {
+                System.err.println("Socket already closed by server \n");
+                inputStream.close();
+                throw new IOException();
+            } else {
+                c = (char) ci;
+                if (c == '#' && canc == false) { // Found first #
+                    response.append(c);
+                    canc = true;
+                } else if (c == '#') { // Found second # command terminated correctly EXIT
+                    response.append(c);
+                    break;
+                } else if (c != '#') { // Append char and start again finding the first #
+                    response.append(c);
+                    canc = false;
+                }
+            }
+        } while (true);
 
-	/**
-	 * Is used to select if the response is a positive ACK
-	 * @param str string to be controlled
-	 * @return true if the message is an ACK
-	 */
-	public static Boolean isACK(final String str){
-		return str.contentEquals("*#*1##");
-	}
-	/**
-	 * Is used to select if the response is a negative ACK
-	 * @param str string to be controlled
-	 * @return true if the message is an NACK
-	 */
-	public static Boolean isNACK(final String str){
-		return str.contentEquals("*#*0##");
-	}
-	
-	/**
-	 * Is used to encode the password for OpenWebNet
-	 * @param str pass password to encode
-	 * @param str nonce key
-	 * @return str encoded passwd
-	*/
-	public static String calcPass(final String pass, final String nonce) {
-		boolean flag = true;
-		int num1 = 0x0;
-		int num2 = 0x0;
-		int password = Integer.parseInt(pass, 10);
-		
-		for (int x=0;x<nonce.length();x++) {
-			char c = nonce.charAt(x);
-			if (c!='0') {
-				if (flag) num2 = password;
-				flag = false;
-			}
-			switch (c) {
-				case '1':
-					num1 = num2 & 0xFFFFFF80;
-					num1 = num1 >>> 7;
-					num2 = num2 << 25;
-					num1 = num1 + num2;
-					break;
-				case '2':
-					num1 = num2 & 0xFFFFFFF0;
-					num1 = num1 >>> 4;
-					num2 = num2 << 28;
-					num1 = num1 + num2;
-					break;
-				case '3':
-					num1 = num2 & 0xFFFFFFF8;
-					num1 = num1 >>> 3;
-					num2 = num2 << 29;
-					num1 = num1 + num2;
-					break;
-				case '4':
-					num1 = num2 << 1;
-					num2 = num2 >>> 31;
-					num1 = num1 + num2;
-					break;
-				case '5':
-					num1 = num2 << 5;
-					num2 = num2 >>> 27;
-					num1 = num1 + num2;
-					break;
-				case '6':
-					num1 = num2 << 12;
-					num2 = num2 >>> 20;
-					num1 = num1 + num2;
-					break;
-				case '7':
-					num1 = num2 & 0x0000FF00;
-					num1 = num1 + (( num2 & 0x000000FF ) << 24 );
-					num1 = num1 + (( num2 & 0x00FF0000 ) >>> 16 );
-					num2 = ( num2 & 0xFF000000 ) >>> 8;
-					num1 = num1 + num2;
-					break;
-				case '8':
-					num1 = num2 & 0x0000FFFF;
-					num1 = num1 << 16;
-					num1 = num1 + ( num2 >>> 24 );
-					num2 = num2 & 0x00FF0000;
-					num2 = num2 >>> 8;
-					num1 = num1 + num2;
-					break;
-				case '9':
-					num1 = ~num2;
-					break;
-				case '0':
-					num1 = num2;
-					break;
-			}
-			num2 = num1;
-		}
-		return Integer.toString(num1 >>> 0);
-	}
+        return response.toString();
+    }
 
-	/**
-	 * Open a command socket with the webserver specified.
-	 * @param ip IP address of the webserver
-	 * @param port of the webserver
-	 * @return the socket ready to be used
-	 * @throws IOException if there is some problem with the socket opening
-	 */
-	public static Socket openCommandSession(final String ip, final int port) throws IOException{
-		Socket sk = new Socket(ip, port);
+    /**
+     * Reads multiple messages from the input stream and returns them back in an array
+     *
+     * @param inputStream steam to read from
+     * @return an array of messages
+     * @throws IOException in case of problem with the input stream, close the stream
+     */
+    protected static String[] readUntilAckNack(final BufferedReader inputStream) throws IOException {
+        ArrayList<String> result = new ArrayList<String>();
+        String commandReceived = null;
+        // Call multiple times the previous function to read more messages.
+        // A sequence of multiple messages end always with an ACK or NACK so stop this cycle when the message is one of
+        // them
+        do {
+            commandReceived = readUntilDelimiter(inputStream);
+            result.add(commandReceived);
+        } while (commandReceived != null && isACK(commandReceived) != true && isNACK(commandReceived) != true);
 
-		BufferedReader inputStream = new BufferedReader(new InputStreamReader(sk.getInputStream()));
-		PrintWriter outputStream = new PrintWriter(sk.getOutputStream(),true);
+        return result.toArray(new String[result.size()]);
+    }
 
-		String response = readUntilDelimiter(inputStream);
+    /**
+     * Is used to select if the response is a positive ACK
+     *
+     * @param str string to be controlled
+     * @return true if the message is an ACK
+     */
+    public static Boolean isACK(final String str) {
+        return str.contentEquals("*#*1##");
+    }
 
-		outputStream.write(socketCommand);
-		outputStream.flush();
+    /**
+     * Is used to select if the response is a negative ACK
+     *
+     * @param str string to be controlled
+     * @return true if the message is an NACK
+     */
+    public static Boolean isNACK(final String str) {
+        return str.contentEquals("*#*0##");
+    }
 
-		response = readUntilDelimiter(inputStream);
+    /**
+     * Is used to encode the password for OpenWebNet
+     *
+     * @param str pass password to encode
+     * @param str nonce key
+     * @return str encoded passwd
+     */
+    public static String calcPass(final String pass, final String nonce) {
+        boolean flag = true;
+        int num1 = 0x0;
+        int num2 = 0x0;
+        int password = Integer.parseInt(pass, 10);
 
-		if (isACK(response) != true) {
-			throw new IOException();
-		}
+        for (int x = 0; x < nonce.length(); x++) {
+            char c = nonce.charAt(x);
+            if (c != '0') {
+                if (flag) {
+                    num2 = password;
+                }
+                flag = false;
+            }
+            switch (c) {
+                case '1':
+                    num1 = num2 & 0xFFFFFF80;
+                    num1 = num1 >>> 7;
+                    num2 = num2 << 25;
+                    num1 = num1 + num2;
+                    break;
+                case '2':
+                    num1 = num2 & 0xFFFFFFF0;
+                    num1 = num1 >>> 4;
+                    num2 = num2 << 28;
+                    num1 = num1 + num2;
+                    break;
+                case '3':
+                    num1 = num2 & 0xFFFFFFF8;
+                    num1 = num1 >>> 3;
+                    num2 = num2 << 29;
+                    num1 = num1 + num2;
+                    break;
+                case '4':
+                    num1 = num2 << 1;
+                    num2 = num2 >>> 31;
+                    num1 = num1 + num2;
+                    break;
+                case '5':
+                    num1 = num2 << 5;
+                    num2 = num2 >>> 27;
+                    num1 = num1 + num2;
+                    break;
+                case '6':
+                    num1 = num2 << 12;
+                    num2 = num2 >>> 20;
+                    num1 = num1 + num2;
+                    break;
+                case '7':
+                    num1 = num2 & 0x0000FF00;
+                    num1 = num1 + ((num2 & 0x000000FF) << 24);
+                    num1 = num1 + ((num2 & 0x00FF0000) >>> 16);
+                    num2 = (num2 & 0xFF000000) >>> 8;
+                    num1 = num1 + num2;
+                    break;
+                case '8':
+                    num1 = num2 & 0x0000FFFF;
+                    num1 = num1 << 16;
+                    num1 = num1 + (num2 >>> 24);
+                    num2 = num2 & 0x00FF0000;
+                    num2 = num2 >>> 8;
+                    num1 = num1 + num2;
+                    break;
+                case '9':
+                    num1 = ~num2;
+                    break;
+                case '0':
+                    num1 = num2;
+                    break;
+            }
+            num2 = num1;
+        }
+        return Integer.toUnsignedString(num1 >>> 0);
+    }
 
-		return sk;
-	}
-	/**
-	 * Open a monitor socket with the webserver specified.
-	 * @param ip IP address of the webserver
-	 * @param port of the webserver
-	 * @param passwd of the gateway
-	 * @return the socket ready to be used
-	 * @throws IOException if there is some problem with the socket opening
-	 */
-	public static Socket openMonitorSession(final String ip, final int port, final String passwd) throws IOException{
-		Socket sk = new Socket(ip, port);
-		sk.setSoTimeout(45*1000);
+    /**
+     * Open a command socket with the webserver specified.
+     *
+     * @param ip IP address of the webserver
+     * @param port of the webserver
+     * @param passwd of the gateway
+     * @return the socket ready to be used
+     * @throws IOException if there is some problem with the socket opening
+     */
+    public static Socket openCommandSession(final String ip, final int port, final String passwd) throws IOException {
+        Socket sk = new Socket(ip, port);
 
-		BufferedReader inputStream = new BufferedReader(new InputStreamReader(sk.getInputStream()));
-		PrintWriter outputStream = new PrintWriter(sk.getOutputStream(),true);
+        BufferedReader inputStream = new BufferedReader(new InputStreamReader(sk.getInputStream()));
+        PrintWriter outputStream = new PrintWriter(sk.getOutputStream(), true);
 
-		String response = readUntilDelimiter(inputStream);
+        String response = readUntilDelimiter(inputStream);
 
-		outputStream.write(socketMonitor);
-		outputStream.flush();
+        outputStream.write(socketCommand);
+        outputStream.flush();
 
-		response = readUntilDelimiter(inputStream);
+        response = readUntilDelimiter(inputStream);
 
-		if (isACK(response) != true) {
-			// check for passwd request
-			String nonce = response.substring(2, response.length()-2);
-			String p = calcPass(passwd, nonce);
-			outputStream.write("*#"+p+"##");
-			outputStream.flush();
+        if (isACK(response) != true) {
+            // check for passwd request
+            String nonce = response.substring(2, response.length() - 2);
+            String p = calcPass(passwd, nonce);
+            outputStream.write("*#" + p + "##");
+            outputStream.flush();
 
-			response = readUntilDelimiter(inputStream);
-			
-			if (isACK(response) != true) {
-				throw new IOException();
-			}
-		}
-		
-		return sk;
-	}
-	/**
-	 * Close the socket passed
-	 * @param sk socket to be closed
-	 * @throws IOException if there is some problem with the socket closure
-	 */
-	public static void disconnect(final Socket sk) throws IOException{
-		sk.close();
-	}
+            response = readUntilDelimiter(inputStream);
+
+            if (isACK(response) != true) {
+                throw new IOException();
+            }
+        }
+
+        return sk;
+    }
+
+    /**
+     * Open a monitor socket with the webserver specified.
+     *
+     * @param ip IP address of the webserver
+     * @param port of the webserver
+     * @param passwd of the gateway
+     * @return the socket ready to be used
+     * @throws IOException if there is some problem with the socket opening
+     */
+    public static Socket openMonitorSession(final String ip, final int port, final String passwd) throws IOException {
+        Socket sk = new Socket(ip, port);
+        sk.setSoTimeout(45 * 1000);
+
+        BufferedReader inputStream = new BufferedReader(new InputStreamReader(sk.getInputStream()));
+        PrintWriter outputStream = new PrintWriter(sk.getOutputStream(), true);
+
+        String response = readUntilDelimiter(inputStream);
+
+        outputStream.write(socketMonitor);
+        outputStream.flush();
+
+        response = readUntilDelimiter(inputStream);
+
+        if (isACK(response) != true) {
+            // check for passwd request
+            String nonce = response.substring(2, response.length() - 2);
+            String p = calcPass(passwd, nonce);
+            outputStream.write("*#" + p + "##");
+            outputStream.flush();
+
+            response = readUntilDelimiter(inputStream);
+
+            if (isACK(response) != true) {
+                throw new IOException();
+            }
+        }
+
+        return sk;
+    }
+
+    /**
+     * Close the socket passed
+     *
+     * @param sk socket to be closed
+     * @throws IOException if there is some problem with the socket closure
+     */
+    public static void disconnect(final Socket sk) throws IOException {
+        sk.close();
+    }
 
 }
